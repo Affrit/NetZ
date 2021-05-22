2c11f9c41c52dbd6ebbb7e5b4e8c8f61151254eb
Affrit <79639177+Affrit@users.noreply.github.com>
2021-05-22 23:00:13 +0300

    L19

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css'
 import Profile from './components/Profile/Profile.js';
 import Header from './components/Header/Header.js';
 import Nav from './components/Nav/Nav.js';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className='app-wrapper'>
       <Header />
       <Nav />
-      <Profile />
+      <div className='app-wrapper-content'>
+        <Profile />
+      </div>
+      <Dialogs />
     </div>
   )
 }
