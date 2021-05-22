9ccd979b3e116980a5bed3917129f4860b511499
Affrit <79639177+Affrit@users.noreply.github.com>
2021-05-22 16:49:26 +0300

    L16

--- src/components/Profile/MyPosts/Posts.js
+++ src/components/Profile/MyPosts/Posts.js
@@ -5,14 +5,17 @@ const Posts = () => {
     return (
         <div className={s.item}>
           New Post
-          <div>
+          <div className={s.newPost}>
             <textarea name="" id="" cols="30" rows="10"></textarea>
             <button>Add post</button>
             <button>delete</button>
           </div>
-          <Post />
-          <Post />
-          <Post />
+          <Post message='Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est magni nobis distinctio eligendi, aliquam mollitia, consectetur quia necessitatibus facere laborum architecto! Dolor odit quidem, nisi esse magnam quas quibusdam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est magni nobis distinctio eligendi, aliquam mollitia, consectetur quia necessitatibus facere laborum architecto! Dolor odit quidem, nisi esse magnam quas quibusdam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est magni nobis distinctio eligendi, aliquam mollitia, consectetur quia necessitatibus facere laborum architecto! Dolor odit quidem, nisi esse magnam quas quibusdam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est magni nobis distinctio eligendi, aliquam mollitia, consectetur quia necessitatibus facere laborum architecto! Dolor odit quidem, nisi esse magnam quas quibusdam.' likes='33' reposts='25'/>
+          <Post message='Wow this is second post' likes='8' reposts='0'/>
+          <Post message="it's realy working!!!" likes='10' reposts='9'/>
         </div>
     )
 }
