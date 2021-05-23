5b5c5b9fb5eef1108249e3df115c11c693ab2d34
Affrit <79639177+Affrit@users.noreply.github.com>
2021-05-24 00:54:45 +0300

    L20

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import Profile from './components/Profile/Profile.js';
 import Header from './components/Header/Header.js';
 import Nav from './components/Nav/Nav.js';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Musik from './components/Musik/Musik';
+import Settings from './components/Settings/Settings';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <div className='app-wrapper-content'>
-        <Profile />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/musik' component={Musik} />
+          <Route path='/settings' component={Settings} />
+        </div>
       </div>
-      <Dialogs />
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Musik/Musik.js
@@ -0,0 +1,11 @@
+import s from './Musik.module.css'
+
+const Musik = (props) => {
+    return (
+        <div>
+            Musik
+        </div>
+    )
+}
+
+export default Musik
