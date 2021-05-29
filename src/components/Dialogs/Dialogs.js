48727aea15892263d6444b7e69dd9116cb48702b
Affrit <79639177+Affrit@users.noreply.github.com>
2021-05-29 15:31:02 +0300

    l25

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -18,20 +18,29 @@ const Messages = (props) => {
     )
 }
 
+const dialogsData = [
+  {id: '1', name: 'Julia'},
+  {id: '2', name: 'Stas'},
+  {id: '3', name: 'Dima'},
+  {id: '4', name: 'Andrey'},
+  {id: '5', name: 'Sergey'},
+]
+
+const messageData = [
+  {id: '1', text: 'Hello'},
+  {id: '2', text: 'How are you?'},
+  {id: '3', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iusto quia nemo, earum nisi autem molestiae? Necessitatibus labore ad voluptatibus odit numquam earum, eaque obcaecati quod, accusantium qui ducimus eligendi?'},
+]
+
+
 const Dialogs = (props) => {
   return (
     <div className={s.dialogsWrapper}>
       <div className={s.dialogs}>
-        <DialogsItem name='Julia' id='1'/>
-        <DialogsItem name='Stas' id='2'/>
-        <DialogsItem name='Dima' id='3'/>
-        <DialogsItem name='Andrey' id='4'/>
-        <DialogsItem name='Sergey' id='5'/>
+        {dialogsData.map(obj => <DialogsItem key={obj.id} name={obj.name} id={obj.id}/>)}
       </div>
       <div className={s.messages}>
-          <Messages text='Hello'/>
-          <Messages text='How are you?'/>
-          <Messages text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iusto quia nemo, earum nisi autem molestiae? Necessitatibus labore ad voluptatibus odit numquam earum, eaque obcaecati quod, accusantium qui ducimus eligendi?'/>
+        {messageData.map(obj => <Messages key={obj.id} text={obj.text}/>)}
       </div>
     </div>
  )
